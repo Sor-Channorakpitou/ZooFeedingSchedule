2fc2765f38350d51073972960f5b92b880bccc93
SorChannorakpitou <channorakpitou.sor@student.cadt.edu.kh>
2026-04-05 10:06:53 +0700

    Improve services and minimize zoo.java

--- src/com/zoo/exceptions/InsufficientFoodException.java
+++
@@ -1,8 +0,0 @@
-package com.zoo.exceptions;
-
-public class InsufficientFoodException extends ZooException {
-    public InsufficientFoodException(String foodName, double needed, double available) {
-        super("Insufficient food: " + foodName + ". Needed: " + needed + "kg, Available: " + available + "kg.");
-    }
-
-}
